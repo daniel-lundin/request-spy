65036af7f932a07b4096cf34c9720885c71b6dfa
Daniel Lundin <d-dog@d-dog.se>
2017-11-30 08:19:38 +0100

    Fix listener leak

--- index.js
+++ index.js
@@ -3,12 +3,22 @@ const monkeypatch = require('monkeypatch');
 
 function spy(cb) {
   monkeypatch(http, 'request', (original, options, callback) => {
-    
-    let startTime = Date.now();
     const clientRequest = original(options, callback);
+    let startTime = Date.now();
+    let requestSocket;
 
-    clientRequest.on('response', (message) => {
+    const errorHandler = (err) => {
+      cb(err, {
+        hostname: options.host || options.hostname,
+        path: options.path,
+        method: options.method
+      });
+    };
+
+
+    clientRequest.once('response', (message) => {
       const requestTime = Date.now() - startTime;
+
       cb(null, {
         hostname: options.host || options.hostname,
         path: options.path,
@@ -16,25 +26,19 @@ function spy(cb) {
         statusCode: message.statusCode,
         requestTime
       });
+      requestSocket.removeListener('error', errorHandler);
     });
 
     clientRequest.on('socket', (socket) => {
-      const requestTime = Date.now() - startTime;
-
-      socket.on('error', (err) => {
-        cb(err, {
-          hostname: options.host || options.hostname,
-          path: options.path,
-          method: options.method,
-          requestTime
-        });
-      });
+      requestSocket = socket;
+      socket.once('error', errorHandler);
     });
 
     monkeypatch(clientRequest, 'end', (original, ...args) => {
       startTime = Date.now();
       original(...args);
     });
+
     return clientRequest;
   });
 }
@@ -47,3 +51,4 @@ module.exports = {
   spy,
   restore
 };
+
